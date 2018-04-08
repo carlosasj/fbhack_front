eba06344eed3e304520cc216403b4f6bbb44cc11
Your Name <otavio.luis.aguiar@gmail.com>
2018-04-07 22:30:32 -0300

    add specialist profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: 'app/question/question.module#QuestionModule'},
   { path: 'specialists', loadChildren: 'app/search-specialist/search-specialist.module#SearchSpecialistModule'},
+  { path: 'specialistprofile', loadChildren: 'app/specialist-screen/specialist-screen.module#SpecialistScreenModule'},
   // { path: '**', redirectTo: 'home'},
 ];
 
